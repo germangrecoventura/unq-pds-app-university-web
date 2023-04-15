3a10465a6d96b26b6beee9cf6c86333baacf52b7
germangrecoventura <germangv1998@gmail.com>
2023-04-15 13:29:45 -0300

    Refactor name

--- src/components/PageComponent/PageComponent.jsx
+++ src/components/PageComponent/PageComponent.jsx
@@ -8,26 +8,26 @@ const PageComponent = (props) => {
       <div className="page-component ">
         <Link to={{ pathname: `/${props.page}/register` }}>
           <button type="button" className="btn btn-primary m-2 btn-lg">
-            Create teacher
+            Create {props.page}
           </button>
         </Link>
         <Link to={{ pathname: `/${props.page}/get` }}>
           <button type="button" className="btn btn-success  m-2 btn-lg">
-            Get teacher
+            Get {props.page}
           </button>
         </Link>
         <Link to={{ pathname: `/${props.page}/update` }}>
           <button type="button" className="btn btn-info m-2 btn-lg">
-            Update teacher
+            Update {props.page}
           </button>
         </Link>
         <Link to={{ pathname: `/${props.page}/delete` }}>
           <button type="button" className="btn btn-danger m-2 btn-lg">
-            Delete teacher
+            Delete {props.page}
           </button>
         </Link>
       </div>
-      <p>Aca debe estar el listado de los docentes</p>
+      <p>Aca debe estar el listado de los {props.page}</p>
     </div>
   );
 };
